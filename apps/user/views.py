7fb9a575e3b5afa2520d607f39010f16ac011454
Tony1sun <jacktony822@gmail.com>
2021-02-02 23:07:14 +0800

    0202晚上

--- apps/user/views.py
+++ apps/user/views.py
@@ -246,7 +246,7 @@ class UserInfoView(LoginRequiredMixin, View):
         user = request.user
         address = Address.objects.get_default_address(user)
 
-        #获取用户的历史浏览记录
+        #获取用户的历史浏览记录1
         # 获取用户的最近浏览
         return render(request, 'user_center_info.html', {'page':'user', 'address': address})
 
