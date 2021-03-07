a3e0baa610316dd0a0514d0ff3537dabd58ea40d
Tony1sun <jacktony822@gmail.com>
2021-03-07 22:18:05 +0800

    0307晚上

--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -6,6 +6,9 @@ import time
 
 import os
 import django
+from goods.models import GoodsType,IndexGoodsBanner,IndexPromotionBanner,IndexTypeGoodsBanner
+from django_redis import get_redis_connection
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dailyfresh.settings")
 django.setup()
 
@@ -37,3 +40,35 @@ def send_register_active_email(to_email, username, token):
 
     send_mail(subject, message, sender, receiver, html_message=html_message)
     time.sleep(5)
+
+def generate_static_index_html():
+    '''产生首页静态页面'''
+    # 获取商品的种类信息
+    types = GoodsType.objects.all()
+
+    # 获取首页轮播商品信息
+    goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+    # 获取首页促销活动信息
+    promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+    # 获取首页分类商品展示信息
+    for type in types:  # GoodsType
+        # 获取type种类首页分类商品的图片展示信息
+        image_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
+        # 获取type种类首页分类商品的文字展示信息
+        title_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
+
+        # 动态给type增加属性，分别保存首页分类商品的图片展示信息和文字展示信息
+        type.image_banners = image_banners
+        type.title_banners = title_banners
+
+
+    # 组织模板上下文
+    context = {'types': types,
+               'goods_banners': goods_banners,
+               'promotion_banners': promotion_banners
+        }
+
+    # 使用模板
+    return render(request, 'index.html', context)
